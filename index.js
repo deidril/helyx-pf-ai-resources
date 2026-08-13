3646e2674f7df6d0e7f782891319469f9931fc91
Deidril <fmanisse@gmail.com>
2026-08-13 16:51:44 +0200

    cotkk wip

---
+++ index.js
@@ -0,0 +1,34 @@
+export const HelyxPFResources ={
+    "name" : "Helyx AI Pathfinder",
+    "directory" : "/modules/helyx-pf-ai-resources/resources",
+
+    "COMPANIONS" : "*******************************************************************************************************",
+
+    "Dog, Black Companion" : { "img":"companions/black-dog_001.actor.webp", "token":"companions/black-dog_001.token.webp"},
+
+    "BESTIARIES ANIMALS" : "*********************************************************************************************",      
+
+    "Bloodseeker" : { "img":"bestiaries/animals/bloodseeker_001.actor.webp", "token":"bestiaries/animals/bloodseeker_001.token.webp"},
+
+    "Snake, Giant Moorsnake" : { "img":"bestiaries/animals/giant-moorsnake_001.actor.webp", "token":"bestiaries/animals/giant-moorsnake_001.token.webp"},
+    
+    "Wolf" : { "img":"bestiaries/animals/wolf_001.actor.webp", "token":"bestiaries/animals/wolf_001.token.webp"},    
+
+    "BESTIARIES CONSTRUCTS" : "********************************************************************************************",
+
+    "Homunculus, Candle" : { "img":"bestiaries/constructs/homunculus_001.actor.webp", "token":"bestiaries/constructs/homunculus_001.token.webp"},
+
+    "BESTIARIES DRAGONS" : "*********************************************************************************************",  
+    
+    "Wyvern" : { "img":"bestiaries/dragons/wyvern_001.actor.webp", "token":"bestiaries/dragons/wyvern_001.token.webp"},
+
+    "BESTIARIES HUMANOIDS" : "**************************************************************************************",     
+
+    "Hobgoblin" : { "img":"bestiaries/humanoids/hobgoblin_001.actor.webp", "token":"bestiaries/humanoids/hobgoblin_001.token.webp"},    
+    
+    "NPCs HUMANS" : "*********************************************************************************************",      
+
+    "Human Hunter" : { "img":"npcs/humans/human-hunter_001.actor.webp", "token":"npcs/humans/human-hunter_001.token.webp"},
+
+    "END" : "***"
+  }
