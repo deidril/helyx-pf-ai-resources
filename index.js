de1e09e721c253b8ac87e89aabcdd8d3a6e6eddb
Deidril <fmanisse@gmail.com>
2026-08-14 16:52:34 +0200

    cotkk wip

--- index.js
+++ index.js
@@ -6,29 +6,78 @@ export const HelyxPFResources ={
 
     "Dog, Black Companion" : { "img":"companions/black-dog_001.actor.webp", "token":"companions/black-dog_001.token.webp"},
 
-    "BESTIARIES ANIMALS" : "*********************************************************************************************",      
+    "BESTIARIES ANIMALS" : "*********************************************************************************************",   
+    
+    "Beetle, Flash" : { "img":"bestiaries/animals/flash-beetle_001.actor.webp", "token":"bestiaries/animals/flash-beetle_001.token.webp"},
+    "Beetle, Giant Stag" : { "img":"bestiaries/animals/giant-stag-beetle_001.actor.webp", "token":"bestiaries/animals/giant-stag-beetle-beetle_001.token.webp"},    
 
     "Bloodseeker" : { "img":"bestiaries/animals/bloodseeker_001.actor.webp", "token":"bestiaries/animals/bloodseeker_001.token.webp"},
 
+    "Lizard, Shocker": { "img":"bestiaries/animals/shocker-lizard_001.actor.webp", "token":"bestiaries/animals/shocker-lizard_001.token.webp"},
+
+    "Rat, Giant" : { "img":"bestiaries/animals/giant-rat_001.actor.webp", "token":"bestiaries/animals/giant-rat_001.token.webp"},    
+
     "Snake, Giant Moorsnake" : { "img":"bestiaries/animals/giant-moorsnake_001.actor.webp", "token":"bestiaries/animals/giant-moorsnake_001.token.webp"},
+
+    "Spider, Hunting" : { "img":"bestiaries/animals/hunting-spider_001.actor.webp", "token":"bestiaries/animals/hunting-spider_001.token.webp"},  
     
     "Wolf" : { "img":"bestiaries/animals/wolf_001.actor.webp", "token":"bestiaries/animals/wolf_001.token.webp"},    
 
     "BESTIARIES CONSTRUCTS" : "********************************************************************************************",
 
+    "Animated Rocking Horse": { "img":"bestiaries/constructs/animated-rocking-horse_001.actor.webp", "token":"bestiaries/constructs/animated-rocking-horse_001.token.webp"},
+    "Animated Statue": { "img":"bestiaries/constructs/animated-statue_001.actor.webp", "token":"bestiaries/constructs/animated-statue_001.token.webp"},    
+
+    "Flying Broom" : { "img":"bestiaries/constructs/flying-broom_001.actor.webp", "token":"bestiaries/constructs/flying-broom_001.token.webp"},    
+
+    "Giant Anvil Pounder" : { "img":"bestiaries/constructs/giant-anvil-pounder_001.actor.webp", "token":"bestiaries/constructs/giant-anvil-pounder_001.token.webp"},      
+
     "Homunculus, Candle" : { "img":"bestiaries/constructs/homunculus_001.actor.webp", "token":"bestiaries/constructs/homunculus_001.token.webp"},
 
+    "Taxidermic Dog" : { "img":"bestiaries/constructs/taxidermic-dog_001.actor.webp", "token":"bestiaries/constructs/taxidermic-dog_001.token.webp"},    
+
     "BESTIARIES DRAGONS" : "*********************************************************************************************",  
+
+    "Tatzlwyrm" : { "img":"bestiaries/dragons/tatzlwyrm_001.actor.webp", "token":"bestiaries/dragons/tatzlwyrm_001.token.webp"},
     
     "Wyvern" : { "img":"bestiaries/dragons/wyvern_001.actor.webp", "token":"bestiaries/dragons/wyvern_001.token.webp"},
 
     "BESTIARIES HUMANOIDS" : "**************************************************************************************",     
 
     "Hobgoblin" : { "img":"bestiaries/humanoids/hobgoblin_001.actor.webp", "token":"bestiaries/humanoids/hobgoblin_001.token.webp"},    
+
+    "BESTIARIES OOZES" : "**************************************************************************************",     
+
+    "Black Pudding" : { "img":"bestiaries/oozes/black-pudding_001.actor.webp", "token":"bestiaries/oozes/black-pudding_001.token.webp"},   
+    
+    "Gelatinous Cube" : { "img":"bestiaries/oozes/gelatinous-cube_001.actor.webp", "token":"bestiaries/oozes/gelatinous-cube_001.token.webp"},   
+
+    "Ochre Jelly" : { "img":"bestiaries/oozes/ochre-jelly_001.actor.webp", "token":"bestiaries/oozes/ochre-jelly_001.token.webp"},     
+
+    "BESTIARIES SWARMS" : "**************************************************************************************",     
+
+    "Swarm, Bats" : { "img":"bestiaries/swarms/bat-swarm_001.actor.webp", "token":"bestiaries/swarms/bat-swarm_001.token.webp"},     
+    
+    "BESTIARIES UNDEADS" : "**************************************************************************************", 
+    
+    "Ghoul" :  { "img":"bestiaries/undeads/ghoul_001.actor.webp", "token":"bestiaries/undeads/ghoul_001.token.webp"},      
+
+    "Shadow" :  { "img":"bestiaries/undeads/shadow_002.actor.webp", "token":"bestiaries/undeads/shadow_002.token.webp"},   
+    "Shadow, Huge" :  { "img":"bestiaries/undeads/shadow_001.actor.webp", "token":"bestiaries/undeads/shadow_001.token.webp"},
+    "Skeleton, Guard" : { "img":"bestiaries/undeads/skeleton-guard_001.actor.webp", "token":"bestiaries/undeads/skeleton-guard_001.token.webp"},      
     
     "NPCs HUMANS" : "*********************************************************************************************",      
 
     "Human Hunter" : { "img":"npcs/humans/human-hunter_001.actor.webp", "token":"npcs/humans/human-hunter_001.token.webp"},
+
+    "NPCs KOBOLDS" : "*********************************************************************************************",      
+
+    "Kobold Bully" : { "img":"npcs/kobolds/kobold-bully_001.actor.webp", "token":"npcs/kobolds/kobold-bully_001.token.webp"},    
+    "Kobold Dark Talon" : { "img":"npcs/kobolds/kobold-dark-talon_001.actor.webp", "token":"npcs/kobolds/kobold-dark-talon_001.token.webp"},    
+    "Kobold Kennel Master" : { "img":"npcs/kobolds/kobold-kennel-master_001.actor.webp", "token":"npcs/kobolds/kobold-kennel-master_001.token.webp"},  
+    "Kobold Mage" : { "img":"npcs/kobolds/kobold-mage_001.actor.webp", "token":"npcs/kobolds/kobold-mage_001.token.webp"},                
+    "Kobold Scout" : { "img":"npcs/kobolds/kobold-scout_001.actor.webp", "token":"npcs/kobolds/kobold-scout_001.token.webp"},  
+    "Kobold Warrior" : { "img":"npcs/kobolds/kobold-warrior_001.actor.webp", "token":"npcs/kobolds/kobold-warrior_001.token.webp"},          
 
     "END" : "***"
   }
