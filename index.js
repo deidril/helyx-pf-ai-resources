af559edcf379aecb14038a707cc57c30b308ebaf
Deidril <fmanisse@gmail.com>
2026-08-21 16:44:07 +0200

    wip

--- index.js
+++ index.js
@@ -14,14 +14,18 @@ export const HelyxPFResources ={
 
     "BESTIARIES ANIMALS" : "*********************************************************************************************",   
     
+    "Bat, Giant Albino" : { "img":"bestiaries/animals/giant-albino-bat_001.actor.webp", "token":"bestiaries/animals/giant-albino-bat_001.token.webp"},
+    "Bear" : { "img":"bestiaries/animals/bear_001.actor.webp", "token":"bestiaries/animals/bear_001.token.webp"},  
     "Beetle, Flash" : { "img":"bestiaries/animals/flash-beetle_001.actor.webp", "token":"bestiaries/animals/flash-beetle_001.token.webp"},
     "Beetle, Giant Stag" : { "img":"bestiaries/animals/giant-stag-beetle_001.actor.webp", "token":"bestiaries/animals/giant-stag-beetle-beetle_001.token.webp"},    
     "Bloodseeker" : { "img":"bestiaries/animals/bloodseeker_001.actor.webp", "token":"bestiaries/animals/bloodseeker_001.token.webp"},
 
     "Crocodile" : { "img":"bestiaries/animals/crocodile_001.actor.webp", "token":"bestiaries/animals/crocodile_001.token.webp"},
 
-    "Frog, Giant" : { "img":"bestiaries/animals/giant-frog_001.actor.webp", "token":"bestiaries/animals/giant-frog_001.token.webp"},    
-
+    "Frog, Giant" : { "img":"bestiaries/animals/giant-frog_001.actor.webp", "token":"bestiaries/animals/giant-frog_001.token.webp"}, 
+    
+    "Hippopotamus" : { "img":"bestiaries/animals/hippopotamus_001.actor.webp", "token":"bestiaries/animals/hippopotamus_001.token.webp"},  
+      
     "Lizard, Shocker": { "img":"bestiaries/animals/shocker-lizard_001.actor.webp", "token":"bestiaries/animals/shocker-lizard_001.token.webp"},
 
     "Mice" : { "img":"bestiaries/animals/mice_001.actor.webp", "token":"bestiaries/animals/mice_001.token.webp"},    
@@ -36,7 +40,13 @@ export const HelyxPFResources ={
     
     "Wolf" : { "img":"bestiaries/animals/wolf_001.actor.webp", "token":"bestiaries/animals/wolf_001.token.webp"},    
 
+    "BESTIARIES, ASURAS" : "*******************************************************************************************************",
+
+    "Nikaramsa" : { "img":"bestiaries/asuras/nikaramsa_001.actor.webp", "token":"bestiaries/asuras/nikaramsa_001.token.webp"},    
+
     "BESTIARIES, BEASTS" : "*******************************************************************************************************",
+
+    "Harpy" : { "img":"bestiaries/beasts/harpy_001.actor.webp", "token":"bestiaries/beasts/harpy_001.token.webp"}, 
 
     "Gargoyle" : { "img":"bestiaries/beasts/gargoyle_001.actor.webp", "token":"bestiaries/beasts/gargoyle_001.token.webp"},        
 
@@ -46,6 +56,8 @@ export const HelyxPFResources ={
 
     "Animated Rocking Horse": { "img":"bestiaries/constructs/animated-rocking-horse_001.actor.webp", "token":"bestiaries/constructs/animated-rocking-horse_001.token.webp"},
     "Animated Statue": { "img":"bestiaries/constructs/animated-statue_001.actor.webp", "token":"bestiaries/constructs/animated-statue_001.token.webp"},    
+
+    "Clockwork Soldier": { "img":"bestiaries/constructs/clockwork-soldier_001.actor.webp", "token":"bestiaries/constructs/clockwork-soldier_001.token.webp"},    
 
     "Flying Broom" : { "img":"bestiaries/constructs/flying-broom_001.actor.webp", "token":"bestiaries/constructs/flying-broom_001.token.webp"},    
 
@@ -57,13 +69,25 @@ export const HelyxPFResources ={
 
     "BESTIARIES DEMONS" : "**************************************************************************************",     
 
-    "Dretch" : { "img":"bestiaries/demons/dretch_001.actor.webp", "token":"bestiaries/demons/dretch_001.token.webp"},      
+    "Barbazu" : { "img":"bestiaries/demons/barbazu_001.actor.webp", "token":"bestiaries/demons/barbazu_001.token.webp"},  
+
+    "Dretch" : { "img":"bestiaries/demons/dretch_001.actor.webp", "token":"bestiaries/demons/dretch_001.token.webp"},     
+    
+    "Glabrezu" : { "img":"bestiaries/demons/glabrezu_001.actor.webp", "token":"bestiaries/demons/glabrezu_001.token.webp"},    
+    
+    "Levaloch" : { "img":"bestiaries/demons/levaloch_001.actor.webp", "token":"bestiaries/demons/levaloch_001.token.webp"}, 
+    
+    "Omox" : { "img":"bestiaries/demons/omox_001.actor.webp", "token":"bestiaries/demons/omox_001.token.webp"},     
 
     "BESTIARIES DRAGONS" : "*********************************************************************************************",  
 
     "Tatzlwyrm" : { "img":"bestiaries/dragons/tatzlwyrm_001.actor.webp", "token":"bestiaries/dragons/tatzlwyrm_001.token.webp"},
     
     "Wyvern" : { "img":"bestiaries/dragons/wyvern_001.actor.webp", "token":"bestiaries/dragons/wyvern_001.token.webp"},
+
+    "BESTIARIES, ELEMENTALS" : "*******************************************************************************************************",
+
+    "Salamander" : { "img":"bestiaries/elementals/salamander_001.actor.webp", "token":"bestiaries/elementals/salamander_001.token.webp"},    
 
     "BESTIARIES HUMANOIDS" : "**************************************************************************************",     
 
@@ -88,7 +112,10 @@ export const HelyxPFResources ={
     "Ghast" :  { "img":"bestiaries/undeads/ghast_002.actor.webp", "token":"bestiaries/undeads/ghast_002.token.webp"},       
     "Ghoul" :  { "img":"bestiaries/undeads/ghoul_001.actor.webp", "token":"bestiaries/undeads/ghoul_001.token.webp"},   
 
-    "Mummy" :  { "img":"bestiaries/undeads/mummy_001.actor.webp", "token":"bestiaries/undeads/mummy_001.token.webp"},         
+    "Muse Phantom" :  { "img":"bestiaries/undeads/muse-phantom_001.actor.webp", "token":"bestiaries/undeads/muse-phantom_001.token.webp"},  
+    "Mummy" :  { "img":"bestiaries/undeads/mummy_001.actor.webp", "token":"bestiaries/undeads/mummy_001.token.webp"},     
+    
+    "Poltergeist" :  { "img":"bestiaries/undeads/poltergeist_001.actor.webp", "token":"bestiaries/undeads/poltergeist_001.token.webp"},     
     
     "Revenant" :  { "img":"bestiaries/undeads/revenant_001.actor.webp", "token":"bestiaries/undeads/revenant_001.token.webp"},     
 
@@ -96,14 +123,25 @@ export const HelyxPFResources ={
     "Shadow, Huge" :  { "img":"bestiaries/undeads/shadow_001.actor.webp", "token":"bestiaries/undeads/shadow_001.token.webp"},
     "Skeleton, Bloody" : { "img":"bestiaries/undeads/bloody-skeleton_001.actor.webp", "token":"bestiaries/undeads/bloody-skeleton_001.token.webp"},     
     "Skeleton, Guard" : { "img":"bestiaries/undeads/skeleton-guard_001.actor.webp", "token":"bestiaries/undeads/skeleton-guard_001.token.webp"},      
-    "Spectre" :  { "img":"bestiaries/undeads/spectre_001.actor.webp", "token":"bestiaries/undeads/spectre_001.token.webp"},         
+    "Spectre" :  { "img":"bestiaries/undeads/spectre_001.actor.webp", "token":"bestiaries/undeads/spectre_001.token.webp"},      
+    "Strigoi, Female" :  { "img":"bestiaries/undeads/strigoi-female_001.actor.webp", "token":"bestiaries/undeads/strigoi-female_001.token.webp"},          
+    "Strigoi, Male" :  { "img":"bestiaries/undeads/strigoi-male_001.actor.webp", "token":"bestiaries/undeads/strigoi-male_001.token.webp"},          
+        
+    "Vampire, 001" :  { "img":"bestiaries/undeads/vampire_001.actor.webp", "token":"bestiaries/undeads/vampire_001.token.webp"},    
+    "Vampire, 002" :  { "img":"bestiaries/undeads/vampire_002.actor.webp", "token":"bestiaries/undeads/vampire_002.token.webp"},
+    "Vampire, Spawn" :  { "img":"bestiaries/undeads/vampire-spawn_001.actor.webp", "token":"bestiaries/undeads/vampire-spawn_001.token.webp"},    
 
     "Zombie, Giant" : { "img":"bestiaries/undeads/giant-zombie_001.actor.webp", "token":"bestiaries/undeads/giant-zombie_001.token.webp"},     
     "Zombie, Owlbear" : { "img":"bestiaries/undeads/zombie-owlbear_001.actor.webp", "token":"bestiaries/undeads/zombie-owlbear_001.token.webp"},         
+    "Zombie, Priest" : { "img":"bestiaries/undeads/zombie-priest_001.actor.webp", "token":"bestiaries/undeads/zombie-priest_001.token.webp"},         
     "Zombie, Shambler Troop" : { "img":"bestiaries/undeads/shambler-troop_001.actor.webp", "token":"bestiaries/undeads/shambler-troop_001.token.webp"},      
     
     "NPCs HUMANS" : "*********************************************************************************************",      
 
+    "Human, Dark Acolyte" : { "img":"npcs/humans/human-dark-acolyte_001.actor.webp", "token":"npcs/humans/human-dark-acolyte_001.token.webp"},      
+    "Human, Dark Priest" : { "img":"npcs/humans/human-dark-priest_001.actor.webp", "token":"npcs/humans/human-dark-priest_001.token.webp"},
+    "Human, Dancer" : { "img":"npcs/humans/human-dancer_001.actor.webp", "token":"npcs/humans/human-dancer_001.token.webp"},
+    "Human Guard" : { "img":"npcs/humans/human-guard_001.actor.webp", "token":"npcs/humans/human-guard_001.token.webp"},
     "Human Hunter" : { "img":"npcs/humans/human-hunter_001.actor.webp", "token":"npcs/humans/human-hunter_001.token.webp"},
 
     "NPCs KOBOLDS" : "*********************************************************************************************",      
@@ -114,6 +152,15 @@ export const HelyxPFResources ={
     "Kobold Mage" : { "img":"npcs/kobolds/kobold-mage_001.actor.webp", "token":"npcs/kobolds/kobold-mage_001.token.webp"},                
     "Kobold Scout" : { "img":"npcs/kobolds/kobold-scout_001.actor.webp", "token":"npcs/kobolds/kobold-scout_001.token.webp"},  
     "Kobold Warrior" : { "img":"npcs/kobolds/kobold-warrior_001.actor.webp", "token":"npcs/kobolds/kobold-warrior_001.token.webp"},          
+
+    "NPCs RAKSHASAS" : "*********************************************************************************************",      
+ 
+    "Rakshasa" : { "img":"npcs/rakshasas/rakshasa_001.actor.webp", "token":"npcs/kobolds/rakshasa_001_001.token.webp"},    
+ 
+    "OTHERS, RUNES" : "*********************************************************************************************",      
+
+    "Rune, Fire" : { "img":"others/runes/fire-rune_001.actor.webp", "token":"others/runes/fire-rune_001_001.token.webp"}, 
+    "Rune, Living" : { "img":"others/runes/living-rune_001.actor.webp", "token":"others/runes/living-rune_001_001.token.webp"}, 
 
     "END" : "***"
   }
